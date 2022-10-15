f000051a7e6c83ab869b578d612369da1e6d21a4
tahataha005 <taha.f.taha0@gmail.com>
2022-10-15 23:53:15 +0300

    Frontend: Adjusting Account.jsx props to accept styling

--- linkedin-frontend/src/components/utilities/Account.jsx
+++ linkedin-frontend/src/components/utilities/Account.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import "../../styles/account.css";
 import image from "../../assets/signup-hero.svg";
 
-const Account = ({ /*image,*/ name, description }) => {
+const Account = ({ /*image,*/ name, description, border }) => {
+    const classes = `account-container flex ${border}`;
     return (
-        <div className="account-container flex">
+        <div className={classes}>
             <div className="profile-img-container white-bg eclipse">
                 <img src={image} alt="" className="eclipse" />
             </div>
