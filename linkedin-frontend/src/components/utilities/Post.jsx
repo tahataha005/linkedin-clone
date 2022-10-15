7f4c4449cc7a1f7255d4b33f4ffaeda6dc2190e5
tahataha005 <taha.f.taha0@gmail.com>
2022-10-15 15:11:41 +0300

    Frontnend: Creating utilities/Post.jsx

---
+++ linkedin-frontend/src/components/utilities/Post.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../styles/utilities.css";
+import image from "../../assets/signup-hero.svg";
+import Account from "./Account";
+
+const Post = ({ title, text }) => {
+    return;
+};
+
+export default Post;
