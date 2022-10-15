1e39e0367839f3eca1d58ffbbac08de7cb502a63
tahataha005 <taha.f.taha0@gmail.com>
2022-10-15 15:14:03 +0300

    Frontnend: Adding structure elements of utilities/Account.jsx

--- linkedin-frontend/src/components/utilities/Account.jsx
+++ linkedin-frontend/src/components/utilities/Account.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import image from "../../assets/signup-hero.svg";
 
 const Account = ({ /*image,*/ name, description }) => {
-    return;
+    return (
+        <div className="account-container flex">
+            <div className="profile-img-container white-bg eclipse">
+                <img src={image} alt="" className="eclipse" />
+            </div>
+            <div className="profile-info-container flex column">
+                <p className="title">{"name"}</p>
+                <p className="text">{"description"}</p>
+            </div>
+        </div>
+    );
 };
 
 export default Account;
