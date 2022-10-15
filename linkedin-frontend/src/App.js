258f45c73a9a81ea4af71533dbb12ae49371a19f
tahataha005 <taha.f.taha0@gmail.com>
2022-10-15 22:00:22 +0300

    Frontend: Setting up routes for all pages

--- linkedin-frontend/src/App.js
+++ linkedin-frontend/src/App.js
@@ -1,13 +1,22 @@
 import "./styles/App.css";
 import Home from "./components/pages/Home";
+import Network from "./components/pages/Network";
+import Notifications from "./components/pages/Notifications";
+import Profile from "./components/pages/Profile";
 import NavBar from "./components/utilities/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
     return (
-        <>
+        <BrowserRouter>
             <NavBar />
-            <Home />
-        </>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/network" element={<Network />} />
+                <Route path="/notifications" element={<Notifications />} />
+                <Route path="/profile" element={<Profile />} />
+            </Routes>
+        </BrowserRouter>
     );
 };
 
