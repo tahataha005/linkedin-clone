6f7ff63e655a7bf977c11d830ec4f46621ebbefd
tahataha005 <taha.f.taha0@gmail.com>
2022-10-15 15:12:40 +0300

    Frontnend: Adding structure elements of utilities/Post.jsx

--- linkedin-frontend/src/components/utilities/Post.jsx
+++ linkedin-frontend/src/components/utilities/Post.jsx
@@ -4,7 +4,16 @@ import image from "../../assets/signup-hero.svg";
 import Account from "./Account";
 
 const Post = ({ title, text }) => {
-    return;
+    return (
+        <div className="post flex column round-edges border white-bg">
+            <Account />
+            <p className="text">{text}</p>
+
+            <div className="image-container border">
+                <img src={image} alt="image" className="post-image" />
+            </div>
+        </div>
+    );
 };
 
 export default Post;
