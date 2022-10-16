8d139433f9ac9717601e0d89c1bd09e732ff3016
tahataha005 <taha.f.taha0@gmail.com>
2022-10-16 18:51:59 +0300

    Backend: Creating user schema in models/user.model.js

---
+++ linkedin-server/models/user.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: "username is required",
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: "password is required",
+        unique: true,
+        trim: true,
+    },
+    location: {
+        type: String,
+    },
+    feild: {
+        type: String,
+    },
+});
