827855cdd230338965abcb4f77f80c2bf3beb790
tahataha005 <taha.f.taha0@gmail.com>
2022-10-16 13:05:02 +0300

    Frontend: Adding structure elements to pages/Jobs.jsx

--- linkedin-frontend/src/components/pages/Jobs.jsx
+++ linkedin-frontend/src/components/pages/Jobs.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-
+import Job from "../utilities/Job.jsx";
+import Section from "../utilities/Section.jsx";
 const Jobs = () => {
-    return <div>Jobs</div>;
+    return (
+        <div className="page-container flex">
+            <Section
+                width={"width-60"}
+                styling={"section round-edges border white-bg"}
+            >
+                <Job />
+            </Section>
+        </div>
+    );
 };
 
 export default Jobs;
