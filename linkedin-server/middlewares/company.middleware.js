ca781d62f735c24de7d2219ef804da50a601c671
tahataha005 <taha.f.taha0@gmail.com>
2022-10-17 00:14:15 +0300

    Backend: Checking authorization in company.middleware.js

--- linkedin-server/middlewares/company.middleware.js
+++ linkedin-server/middlewares/company.middleware.js
@@ -1,3 +1,9 @@
-const companyMiddleware = async (req, res, next) => {};
+const companyMiddleware = async (req, res, next) => {
+    if (req.user.user_type === "2") {
+        next();
+    } else {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+};
 
 module.exports = companyMiddleware;
