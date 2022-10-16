80edbd536d1b87ff50b8266a72cc151c395c5013
tahataha005 <taha.f.taha0@gmail.com>
2022-10-16 21:45:56 +0300

    Backend: Creating getInfo function in user.controller.js

--- linkedin-server/controllers/user.controller.js
+++ linkedin-server/controllers/user.controller.js
@@ -1,1 +1,9 @@
 const User = require("../models/user.model.js");
+
+const getInfo = async (req, res) => {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.json({ user });
+};
+
+module.exports = { getInfo };
