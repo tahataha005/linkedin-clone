0401f8c6241ccf197648f65e8546734c31a2fc6b
tahataha005 <taha.f.taha0@gmail.com>
2022-10-16 16:42:12 +0300

    Backend: Requiring dotenv and starting the server in index.js

--- linkedin-server/index.js
+++ linkedin-server/index.js
@@ -1,2 +1,12 @@
 const express = require("express");
 const app = express();
+
+require("dotenv").config();
+
+app.listen(process.env.PORT, err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    }
+});
