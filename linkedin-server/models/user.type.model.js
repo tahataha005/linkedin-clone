fbc624e57c6cf139348469e942fa60ae494ba94e
tahataha005 <taha.f.taha0@gmail.com>
2022-10-17 00:31:53 +0300

    Backend: Creating user type schema

--- linkedin-server/models/user.type.model.js
+++ linkedin-server/models/user.type.model.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose");
-const user_typeSchema = mongoose.Schema({});
+const user_typeSchema = mongoose.Schema({
+    user_type: {
+        type: String,
+        enum: ["user", "company"],
+        required: true,
+    },
+});
 
 const UserType = mongoose.model("UserType", user_typeSchema);
 
