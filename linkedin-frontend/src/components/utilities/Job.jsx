0808d579b728a5a4a41065c9747dc23c8ea109a0
tahataha005 <taha.f.taha0@gmail.com>
2022-10-16 13:05:59 +0300

    Frontend: Adding structure elements to utilities/Job.jsx

--- linkedin-frontend/src/components/utilities/Job.jsx
+++ linkedin-frontend/src/components/utilities/Job.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import image from "../../assets/signup-hero.svg";
+import "../../styles/job.css";
 
 const Job = () => {
-    return <div>Job</div>;
+    return (
+        <div className="job-container flex">
+            <div className="profile-img-container">
+                <img src={image} alt="" />
+            </div>
+            <div className="job-info flex column">
+                <p className="title blue-txt">Job Name</p>
+                <p className="text">Company name</p>
+                <p className="text light-txt">Work</p>
+            </div>
+        </div>
+    );
 };
 
 export default Job;
