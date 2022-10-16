c850580178e82df019a5f3bb2923190c48f6fe7b
tahataha005 <taha.f.taha0@gmail.com>
2022-10-16 19:14:24 +0300

    Backend: Importing login and signup functions from auth.controller.js

--- linkedin-server/routes/auth.route.js
+++ linkedin-server/routes/auth.route.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 
 const router = Router();
 
+const { login, signup } = require("../controllers/auth.controller.js");
+
 router.post("/login", login);
 router.post("signup", signup);
 
