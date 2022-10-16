b3ba5628fb0f2707c97d21c99ea999ac7d25e93c
tahataha005 <taha.f.taha0@gmail.com>
2022-10-16 23:52:19 +0300

    Backend: Adding auth.middleware to requests

--- linkedin-server/routes/user.route.js
+++ linkedin-server/routes/user.route.js
@@ -3,8 +3,9 @@ const { Router } = require("express");
 const router = new Router();
 
 const { getInfo, editInfo } = require("../controllers/user.controller.js");
+const authMiddleware = require("../middlewares/auth.middleware.js");
 
-router.get("/:id", getInfo);
-router.put("/", editInfo);
+router.get("/:id", authMiddleware, getInfo);
+router.put("/", authMiddleware, editInfo);
 
 module.exports = router;
