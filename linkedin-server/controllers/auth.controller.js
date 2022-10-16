37542ed2ec00d0da400d3e9439c6c4018e0c8bd8
tahataha005 <taha.f.taha0@gmail.com>
2022-10-16 20:36:53 +0300

    Backend: Creating a new user in signup function (no jwt)

--- linkedin-server/controllers/auth.controller.js
+++ linkedin-server/controllers/auth.controller.js
@@ -1,22 +1,27 @@
 const User = require("../models/user.model.js");
+const bcrypt = require("bcrypt");
 
 const login = (req, res) => {
     res.send("login");
 };
 
-const signup = (req, res) => {
-    const { name, email, password, user_type, location, feild } = req.body;
-    const user = new User();
+const signup = async (req, res) => {
+    const { name, email, password, user_type, location, field } = req.body;
+    try {
+        const user = new User();
 
-    user.name = name;
-    user.email = email;
-    user.password = password;
-    user.user_type = user_type;
-    user.location = location;
-    user.feild = feild;
+        user.name = name;
+        user.email = email;
+        user.password = await bcrypt.hash(password, 10);
+        user.user_type = user_type;
+        user.location = location;
+        user.field = field;
 
-    user.save();
-    res.json({ user: user });
+        await user.save();
+        res.json({ message: "success", id: user.id });
+    } catch (err) {
+        res.status(400).send({ message: err.message });
+    }
 };
 
 module.exports = { login, signup };
