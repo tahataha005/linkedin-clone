bdb0908bb8d8f8752ac343b4576aab2572381bdf
tahataha005 <taha.f.taha0@gmail.com>
2022-10-16 20:34:09 +0300

    Backend: Installing bcrypt package

--- linkedin-server/controllers/auth.controller.js
+++ linkedin-server/controllers/auth.controller.js
@@ -1,7 +1,22 @@
 const User = require("../models/user.model.js");
 
-const login = (req, res) => {};
+const login = (req, res) => {
+    res.send("login");
+};
 
-const signup = (req, res) => {};
+const signup = (req, res) => {
+    const { name, email, password, user_type, location, feild } = req.body;
+    const user = new User();
+
+    user.name = name;
+    user.email = email;
+    user.password = password;
+    user.user_type = user_type;
+    user.location = location;
+    user.feild = feild;
+
+    user.save();
+    res.json({ user: user });
+};
 
 module.exports = { login, signup };
