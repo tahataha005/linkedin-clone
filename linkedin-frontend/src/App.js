a3904f62edfc9ed57412777c389ccb7595a1f659
tahataha005 <taha.f.taha0@gmail.com>
2022-10-16 11:26:55 +0300

    Frontend: Adding route for Jobs page in src/App.js

--- linkedin-frontend/src/App.js
+++ linkedin-frontend/src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/pages/Home";
 import Network from "./components/pages/Network";
 import Notifications from "./components/pages/Notifications";
 import Profile from "./components/pages/Profile";
+import Jobs from "./components/pages/Jobs";
 import NavBar from "./components/utilities/NavBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -15,6 +16,7 @@ const App = () => {
                 <Route path="/network" element={<Network />} />
                 <Route path="/notifications" element={<Notifications />} />
                 <Route path="/profile" element={<Profile />} />
+                <Route path="/jobs" element={<Jobs />} />
             </Routes>
         </BrowserRouter>
     );
