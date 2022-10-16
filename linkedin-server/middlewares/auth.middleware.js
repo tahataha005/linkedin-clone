19a35b92036452a2ee33a4da403cedce1cd924af
tahataha005 <taha.f.taha0@gmail.com>
2022-10-16 23:51:01 +0300

    Backend: Adding authentication proccess to auth.middleware.js

--- linkedin-server/middlewares/auth.middleware.js
+++ linkedin-server/middlewares/auth.middleware.js
@@ -1,5 +1,21 @@
+const User = require("../models/user.model.js");
 const jwt = require("jsonwebtoken");
 
-const authMiddleware = (req, res, next) => {};
+const authMiddleware = async (req, res, next) => {
+    const token = req.headers.authorization.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        const user = await User.findOne({ email: decoded.email });
+        res.user = user;
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: err.message });
+    }
+};
 
 module.exports = authMiddleware;
