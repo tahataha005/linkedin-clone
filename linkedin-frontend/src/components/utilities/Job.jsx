055367011007c3d0dd0aef9d6ae3081e0708bf86
tahataha005 <taha.f.taha0@gmail.com>
2022-10-16 15:33:22 +0300

    Frontend: Adding props to elements in utilities/Job.jsx

--- linkedin-frontend/src/components/utilities/Job.jsx
+++ linkedin-frontend/src/components/utilities/Job.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import image from "../../assets/signup-hero.svg";
 import "../../styles/job.css";
 
-const Job = () => {
+const Job = ({ job, company, location, work, applicants }) => {
     return (
         <div className="job-container flex">
             <div className="profile-img-container">
                 <img src={image} alt="" />
             </div>
             <div className="job-info flex column">
-                <p className="title blue-txt">Job Name</p>
-                <p className="text">Company name</p>
-                <p className="text light-txt">Work</p>
+                <p className="title blue-txt">{job}</p>
+                <p className="text">{company}</p>
+                <p className="text light-txt">{`${location} (${work})`}</p>
+                <p className="text light-txt">{`Applicants: ${applicants}`}</p>
             </div>
         </div>
     );
