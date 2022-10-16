f143814432d79a94bfbb3020b6e1bbc31278d368
tahataha005 <taha.f.taha0@gmail.com>
2022-10-16 19:13:27 +0300

    Backend: Importing and using authRoutes

--- linkedin-server/index.js
+++ linkedin-server/index.js
@@ -3,6 +3,10 @@ require("dotenv").config();
 require("./config/db.config.js");
 
 const app = express();
+app.use(express.json());
+
+const authRoutes = require("./routes/auth.route.js");
+app.use("/auth", authRoutes);
 
 app.listen(process.env.PORT, err => {
     if (err) {
