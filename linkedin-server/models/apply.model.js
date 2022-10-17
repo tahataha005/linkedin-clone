6e6ed553c5d73c547f78f38818c63d98063f0be4
tahataha005 <taha.f.taha0@gmail.com>
2022-10-17 10:02:40 +0300

    Backend: Creating Job model

--- linkedin-server/models/apply.model.js
+++ linkedin-server/models/apply.model.js
@@ -1,5 +1,18 @@
 const mongoose = require("mongoose");
 
+const applySchema = mongoose.Schema({
+    user_id: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    job_id: {
+        type: mongoose.Types.ObjectId,
+        ref: "Job",
+        required: true,
+    },
+});
+
 const Apply = mongoose.model("Apply", applySchema);
 
 module.exports = Apply;
