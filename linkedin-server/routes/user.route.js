f23868c5ef64e32fd758eb5d7f54fca6c6cf96aa
tahataha005 <taha.f.taha0@gmail.com>
2022-10-17 09:50:51 +0300

    Backend: Creating a route to add jobs in user.routes.js

--- linkedin-server/routes/user.route.js
+++ linkedin-server/routes/user.route.js
@@ -2,10 +2,16 @@ const { Router } = require("express");
 
 const router = new Router();
 
-const { getInfo, editInfo } = require("../controllers/user.controller.js");
+const {
+    getInfo,
+    editInfo,
+    addJob,
+} = require("../controllers/user.controller.js");
 const authMiddleware = require("../middlewares/auth.middleware.js");
+const companyMiddleware = require("../middlewares/company.middleware.js");
 
 router.get("/:id", authMiddleware, getInfo);
 router.put("/", authMiddleware, editInfo);
+router.post("/job", authMiddleware, companyMiddleware, addJob);
 
 module.exports = router;
