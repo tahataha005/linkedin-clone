c9ea15b4557103885a4916176e96a89261ab3408
tahataha005 <taha.f.taha0@gmail.com>
2022-10-17 10:21:55 +0300

    Backend: Creating route to apply to jobs in user.route

--- linkedin-server/routes/user.route.js
+++ linkedin-server/routes/user.route.js
@@ -6,12 +6,15 @@ const {
     getInfo,
     editInfo,
     addJob,
+    apply,
 } = require("../controllers/user.controller.js");
 const authMiddleware = require("../middlewares/auth.middleware.js");
 const companyMiddleware = require("../middlewares/company.middleware.js");
+const userMiddleware = require("../middlewares/user.middleware.js");
 
 router.get("/:id", authMiddleware, getInfo);
 router.put("/", authMiddleware, editInfo);
 router.post("/job", authMiddleware, companyMiddleware, addJob);
+router.post("/apply", authMiddleware, userMiddleware, apply);
 
 module.exports = router;
