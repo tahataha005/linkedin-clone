b2155722008381c4c1c8392dc1f1e25ea9feb8f1
tahataha005 <taha.f.taha0@gmail.com>
2022-10-17 09:14:02 +0300

    Backend: Creating job schema and job model

--- linkedin-server/models/job.model.js
+++ linkedin-server/models/job.model.js
@@ -1,3 +1,24 @@
 const mongoose = require("mongoose");
 
+const jobSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    work: {
+        type: String,
+        enum: ["Online", "OnSight"],
+    },
+    company_id: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+});
+
+const Job = mongoose.model("Job", jobSchema);
+
 module.exports = Job;
