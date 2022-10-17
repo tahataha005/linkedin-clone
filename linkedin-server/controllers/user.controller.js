7db83c7d51aee5052e66ed7aa0e590fb10fc4fd4
tahataha005 <taha.f.taha0@gmail.com>
2022-10-17 10:21:22 +0300

    Backend: Creating apply function to apply to jobs in user.controller.js

--- linkedin-server/controllers/user.controller.js
+++ linkedin-server/controllers/user.controller.js
@@ -1,5 +1,6 @@
 const User = require("../models/user.model.js");
 const Job = require("../models/job.model.js");
+const Apply = require("../models/apply.model.js");
 
 const getInfo = async (req, res) => {
     const { id } = req.params;
@@ -18,9 +19,10 @@ const editInfo = async (req, res) => {
 
 const addJob = async (req, res) => {
     const { company_id, name, description, work } = req.body;
-    const job = new Job();
 
     try {
+        const job = new Job();
+
         job.name = name;
         job.description = description;
         job.work = work;
@@ -33,4 +35,20 @@ const addJob = async (req, res) => {
     }
 };
 
-module.exports = { addJob, getInfo, editInfo };
+const apply = async (req, res) => {
+    const { user_id, job_id } = req.body;
+
+    try {
+        const apply = new Apply();
+
+        apply.user_id = user_id;
+        apply.job_id = job_id;
+
+        await apply.save();
+        res.json(apply);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+module.exports = { addJob, getInfo, editInfo, apply };
