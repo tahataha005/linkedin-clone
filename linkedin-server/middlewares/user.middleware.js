31a95fa6c69e673556f919ee3b3a4c6336481792
tahataha005 <taha.f.taha0@gmail.com>
2022-10-17 09:25:58 +0300

    Backend: Changing user types ids and adding them to .env

--- linkedin-server/middlewares/user.middleware.js
+++ linkedin-server/middlewares/user.middleware.js
@@ -1,5 +1,5 @@
 const userMiddleware = async (req, res, next) => {
-    if (req.user.user_type === "1") {
+    if (req.user.user_type === process.env.TYPE_USER_ID) {
         next();
     } else {
         return res.status(401).json({ message: "Unauthorized" });
