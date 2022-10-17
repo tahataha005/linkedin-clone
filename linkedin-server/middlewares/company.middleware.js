a47ccf90cd6474edb11608da0961feeecbdbf570
tahataha005 <taha.f.taha0@gmail.com>
2022-10-17 21:05:43 +0300

    Backend: Comparing usertype directly in company.middleware

--- linkedin-server/middlewares/company.middleware.js
+++ linkedin-server/middlewares/company.middleware.js
@@ -1,5 +1,5 @@
 const companyMiddleware = async (req, res, next) => {
-    if (req.user.user_type == process.env.TYPE_COMPANY_ID) {
+    if (req.user.user_type.id === "634c79ea0432f5d4f9a72769") {
         next();
     } else {
         return res.status(401).json({ message: "Unauthorized" });
