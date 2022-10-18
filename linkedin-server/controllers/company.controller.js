4557f5f72a0fc4e326e07e4c9c68de106b4d6a6c
tahataha005 <taha.f.taha0@gmail.com>
2022-10-18 09:14:31 +0300

    Frontend: Creating company.controller.js

---
+++ linkedin-server/controllers/company.controller.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const User = require("../models/user.model.js");
+const Post = require("../models/post.model.js");
+const Job = require("../models/job.model.js");
+const Apply = require("../models/apply.model.js");
+
+module.exports = {};
