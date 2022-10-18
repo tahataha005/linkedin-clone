5296bdc7d7fcd5b9470b1f3bc869080889a62f49
tahataha005 <taha.f.taha0@gmail.com>
2022-10-18 08:57:36 +0300

    Frontend: Adding a route for getJobs function in user.route.js

--- linkedin-server/routes/user.route.js
+++ linkedin-server/routes/user.route.js
@@ -9,6 +9,7 @@ const {
     apply,
     addPost,
     getPosts,
+    getJobs,
 } = require("../controllers/user.controller.js");
 
 const authMiddleware = require("../middlewares/auth.middleware.js");
@@ -19,6 +20,7 @@ router.get("/post", authMiddleware, getPosts);
 router.get("/:id", authMiddleware, getInfo);
 router.put("/", authMiddleware, editInfo);
 router.post("/job", authMiddleware, companyMiddleware, addJob);
+router.get("/job", authMiddleware, companyMiddleware, getJobs);
 router.post("/apply", authMiddleware, userMiddleware, apply);
 router.post("/post", authMiddleware, addPost);
 
