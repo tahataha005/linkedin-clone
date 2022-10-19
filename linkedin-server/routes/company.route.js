5458bfe6059e7491c258a4710366992e10b73127
tahataha005 <taha.f.taha0@gmail.com>
2022-10-19 10:57:10 +0300

    Backend: Creating a route for getJobs function in company.routes.js

--- linkedin-server/routes/company.route.js
+++ linkedin-server/routes/company.route.js
@@ -3,10 +3,8 @@ const { Router } = require("express");
 const router = new Router();
 
 const { addJob, getJobs } = require("../controllers/company.controller.js");
-const companyMiddleware = require("../middlewares/company.middleware.js");
-const authMiddleware = require("../middlewares/auth.middleware.js");
 
-router.get("/job", authMiddleware, addJob);
-router.get("/jobs/:company_id", authMiddleware, companyMiddleware, getJobs);
+router.get("/job", addJob);
+router.get("/jobs/:company_id", getJobs);
 
 module.exports = router;
