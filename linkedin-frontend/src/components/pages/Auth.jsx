a48ed6e7890d2bd8fc9f39c893e5e5d2102aac9d
tahataha005 <taha.f.taha0@gmail.com>
2022-10-19 11:57:30 +0300

    Frontend: Adding structure elements to Auth.jsx

--- linkedin-frontend/src/components/pages/Auth.jsx
+++ linkedin-frontend/src/components/pages/Auth.jsx
@@ -4,7 +4,29 @@ import Section from "../utilities/Section";
 import Button from "../utilities/Button";
 
 const Auth = () => {
-    return <div className="page-container white-bg flex row"></div>;
+    return (
+        <div className="page-container white-bg flex row">
+            <Section width={"width-50"} styling={"white-bg"}>
+                <div>
+                    <p>Join the biggest professional community</p>
+                    <div>
+                        <Input />
+                        <Input />
+                    </div>
+                    <Button />
+                    <div>
+                        <p>Already have an account?</p>
+                        <Button />
+                    </div>
+                </div>
+            </Section>
+            <Section width={"width-50"} styling={"white-bg"}>
+                <div>
+                    <img src="" alt="" />
+                </div>
+            </Section>
+        </div>
+    );
 };
 
 export default Auth;
