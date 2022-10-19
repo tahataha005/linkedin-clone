e29088f00ee9d1fdd38d3d4d198c7a0327bc5bc1
tahataha005 <taha.f.taha0@gmail.com>
2022-10-19 13:52:24 +0300

    Frontend: Displaying sign up content according to boolean variable in pages/Auth.jsx

--- linkedin-frontend/src/components/pages/Auth.jsx
+++ linkedin-frontend/src/components/pages/Auth.jsx
@@ -3,45 +3,61 @@ import "../../styles/auth.css";
 import Input from "../utilities/Input";
 import Section from "../utilities/Section";
 import Button from "../utilities/Button";
+import image from "../../assets/signup-hero.svg";
+import { useState } from "react";
 
 const Auth = () => {
+    const [signup, setSignup] = useState(false);
     return (
-        <div className="page-container white-bg flex row">
-            <Section width={"width-50"} styling={"white-bg"}>
-                <div className="right-content flex">
-                    <div className="auth-block">
-                        <p className="landing-title">
-                            Join the biggest professional community
-                        </p>
-                        <div className="auth-inputs-container flex column">
-                            <Input
-                                type={"auth-input border"}
-                                placeholder={"Email"}
-                            />
-                            <Input
-                                type={"auth-input border"}
-                                placeholder={"Password"}
+        <div className="page-container flex">
+            {signup && (
+                <div className="signup-container white-bg flex row wrap">
+                    <Section
+                        width={"width-50"}
+                        styling={"white-bg min-width-500"}
+                    >
+                        <div className="content flex">
+                            <div className="auth-block">
+                                <p className="landing-title">
+                                    Join the biggest professional community
+                                </p>
+                                <div className="auth-inputs-container flex column">
+                                    <Input
+                                        type={"auth-input border"}
+                                        placeholder={"Email"}
+                                    />
+                                    <Input
+                                        type={"auth-input border"}
+                                        placeholder={"Password"}
+                                    />
+                                </div>
+                                <Button
+                                    type={"auth-button dark-bg white-txt"}
+                                    text={"SIGN IN"}
+                                />
+                                <div className="auth-switch-container flex row">
+                                    <p>Already have an account?</p>
+                                    <Button
+                                        type={
+                                            "auth-switch-button blue-txt white-bg"
+                                        }
+                                        text={"Sign Up"}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </Section>
+                    <Section width={"width-50"} styling={"white-bg"}>
+                        <div className="content">
+                            <img
+                                className="hero-image"
+                                src={image}
+                                alt={image}
                             />
                         </div>
-                        <Button
-                            type={"auth-button dark-bg white-txt"}
-                            text={"SIGN IN"}
-                        />
-                        <div className="auth-switch-container flex row">
-                            <p>Already have an account?</p>
-                            <Button
-                                type={"auth-switch-button blue-txt white-bg"}
-                                text={"Sign Up"}
-                            />
-                        </div>
-                    </div>
+                    </Section>
                 </div>
-            </Section>
-            <Section width={"width-50"} styling={"white-bg"}>
-                <div>
-                    <img src="" alt="" />
-                </div>
-            </Section>
+            )}
         </div>
     );
 };
