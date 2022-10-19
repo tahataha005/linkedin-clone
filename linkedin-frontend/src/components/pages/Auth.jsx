95ae0b20cd9c501ff1f6e3dee61dc9a352009896
tahataha005 <taha.f.taha0@gmail.com>
2022-10-19 11:56:18 +0300

    Frontend: Creating pages/Auth.jsx

---
+++ linkedin-frontend/src/components/pages/Auth.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Input from "../utilities/Input";
+import Section from "../utilities/Section";
+import Button from "../utilities/Button";
+
+const Auth = () => {
+    return <div className="page-container white-bg flex row"></div>;
+};
+
+export default Auth;
