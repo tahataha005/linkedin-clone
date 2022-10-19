420f8946db8517ac4ecbd56f4f9a8c5cf1707c4f
tahataha005 <taha.f.taha0@gmail.com>
2022-10-19 13:14:51 +0300

    Frontend: Giving claases to right section of pages/Auth.jsx

--- linkedin-frontend/src/components/pages/Auth.jsx
+++ linkedin-frontend/src/components/pages/Auth.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../styles/auth.css";
 import Input from "../utilities/Input";
 import Section from "../utilities/Section";
 import Button from "../utilities/Button";
@@ -7,16 +8,32 @@ const Auth = () => {
     return (
         <div className="page-container white-bg flex row">
             <Section width={"width-50"} styling={"white-bg"}>
-                <div>
-                    <p>Join the biggest professional community</p>
-                    <div>
-                        <Input />
-                        <Input />
-                    </div>
-                    <Button />
-                    <div>
-                        <p>Already have an account?</p>
-                        <Button />
+                <div className="right-content flex">
+                    <div className="auth-block">
+                        <p className="landing-title">
+                            Join the biggest professional community
+                        </p>
+                        <div className="auth-inputs-container flex column">
+                            <Input
+                                type={"auth-input border"}
+                                placeholder={"Email"}
+                            />
+                            <Input
+                                type={"auth-input border"}
+                                placeholder={"Password"}
+                            />
+                        </div>
+                        <Button
+                            type={"auth-button dark-bg white-txt"}
+                            text={"SIGN IN"}
+                        />
+                        <div className="auth-switch-container flex row">
+                            <p>Already have an account?</p>
+                            <Button
+                                type={"auth-switch-button blue-txt white-bg"}
+                                text={"Sign Up"}
+                            />
+                        </div>
                     </div>
                 </div>
             </Section>
