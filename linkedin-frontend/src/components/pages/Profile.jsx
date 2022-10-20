bcb6a05134eccdc7d51a00d1de462b504d7a5b25
tahataha005 <taha.f.taha0@gmail.com>
2022-10-20 18:46:14 +0300

    Frontend: Getting id from local storage in pages/Profile.jsx

--- linkedin-frontend/src/components/pages/Profile.jsx
+++ linkedin-frontend/src/components/pages/Profile.jsx
@@ -4,20 +4,27 @@ import Account from "../utilities/Account";
 import { useState, useEffect } from "react";
 import sendRequest from "../../config/axios";
 
-const Profile = () => {
+const Profile = ({ id }) => {
     const [user, setUser] = useState();
 
     const getUser = async () => {
         const user = await sendRequest({
-            route: "/user/634ced20f805eea4fea92bd3",
+            route: `/user/${
+                localStorage.getItem("clicked_id")
+                    ? localStorage.getItem("clicked_id")
+                    : localStorage.getItem("id")
+            }`,
         });
-        setUser(user);
+        setUser(user.user);
     };
 
     useEffect(() => {
         getUser();
     }, []);
 
+    useEffect(() => {
+        console.log(user);
+    }, [user]);
     return (
         <div className="page-container flex">
             <Section
