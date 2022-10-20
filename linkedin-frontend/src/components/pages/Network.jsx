98a932b10cc13efaeb146885a2777bff020074be
tahataha005 <taha.f.taha0@gmail.com>
2022-10-21 01:31:56 +0300

    Frontend: Fetching for following in pages/Network.js

--- linkedin-frontend/src/components/pages/Network.jsx
+++ linkedin-frontend/src/components/pages/Network.jsx
@@ -1,29 +1,35 @@
 import React from "react";
 import Section from "../../components/utilities/Section.jsx";
 import Account from "../../components/utilities/Account.jsx";
+import sendRequest from "../../config/axios.js";
+import { useEffect, useState } from "react";
 
 const Network = () => {
-    const followed = [
-        { id: 1, name: "SE Factory", description: "Software Development" },
-        { id: 2, name: "Taha's Company", description: "Education and Stuff" },
-        { id: 3, name: "Company", description: "Description Example" },
-        {
-            id: 4,
-            name: "Ghandour",
-            description: "Lebanese Company of Biscuits",
-        },
-    ];
+    const [followed, setFollowed] = useState();
+
+    useEffect(() => {
+        const getFollowing = async () => {
+            const response = await sendRequest({
+                route: `/user/follow/${localStorage.getItem("id")}`,
+            });
+            setFollowed(response);
+        };
+
+        getFollowing();
+    }, []);
+
     return (
         <div className="page-container flex">
             <Section
                 width={"width-60"}
                 styling={"section round-edges border white-bg"}
             >
-                {followed.map(company => {
+                {followed?.map(company => {
                     return (
                         <Account
-                            name={company.name}
-                            description={company.description}
+                            key={company.company_id._id}
+                            name={company.company_id.email}
+                            description={company.company_id.field}
                             styling={"account-border"}
                             profile_pic={"profile-img-container"}
                         />
