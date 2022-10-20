4f507692c204a1761c859d0d3a392c55ca340b9d
tahataha005 <taha.f.taha0@gmail.com>
2022-10-20 21:28:31 +0300

    Development: Creating Follow model schema

--- linkedin-server/models/follow.model.js
+++ linkedin-server/models/follow.model.js
@@ -1,5 +1,18 @@
 const mongoose = require("mongoose");
 
+const followSchema = mongoose.Schema({
+    user_id: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    company_id: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+});
+
 const Follow = mongoose.model("Follow", followSchema);
 
 module.exports = Follow;
