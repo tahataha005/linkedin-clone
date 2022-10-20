823846722acc281bffc3fe31257b50a4421ef92d
tahataha005 <taha.f.taha0@gmail.com>
2022-10-21 01:58:16 +0300

    Frontend: Adding apply button and sending application to database in utilities/Job.jsx

--- linkedin-frontend/src/components/utilities/Job.jsx
+++ linkedin-frontend/src/components/utilities/Job.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import image from "../../assets/signup-hero.svg";
 import "../../styles/job.css";
+import sendRequest from "../../config/axios";
+import Button from "../utilities/Button.jsx";
 
-const Job = ({ job, company, location, work, applicants }) => {
+const Job = ({ job_id, job, company, location, work, applicants }) => {
+    const apply = async () => {
+        const data = {
+            user_id: localStorage.getItem("id"),
+            job_id: job_id,
+        };
+        const response = await sendRequest({
+            route: `/user/apply`,
+            data: data,
+            method: "post",
+        });
+        console.log(response);
+    };
+
     return (
         <div className="job-container flex">
             <div className="profile-img-container">
@@ -14,6 +29,11 @@ const Job = ({ job, company, location, work, applicants }) => {
                 <p className="text light-txt">{`${location} (${work})`}</p>
                 <p className="text light-txt">{`Applicants: ${applicants}`}</p>
             </div>
+            <Button
+                type={"regular-btn"}
+                text={"Apply"}
+                onClick={() => apply()}
+            />
         </div>
     );
 };
