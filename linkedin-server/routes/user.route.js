19ad41b6dd9194598016723be5ddfa1c426c9bc4
tahataha005 <taha.f.taha0@gmail.com>
2022-10-21 01:14:19 +0300

    Backend: Creating route for getFollowing function in user.route

--- linkedin-server/routes/user.route.js
+++ linkedin-server/routes/user.route.js
@@ -10,10 +10,12 @@ const {
     getPosts,
     getJobs,
     follow,
+    getFollowing,
 } = require("../controllers/user.controller.js");
 
 router.get("/post", getPosts);
 router.post("/post", addPost);
+router.get("/follow/:id", getFollowing);
 router.get("/:id", getInfo);
 router.put("/", editInfo);
 router.get("/job", getJobs);
